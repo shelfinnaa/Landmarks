2ea50a2be89efe95c50e766cc1fb095af13399ee
Shelfinna <shelfinna.s@gmail.com>
2024-04-26 08:33:50 +0700

    Chapter 2: Section 6

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -9,42 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            
-            // Menampilkan peta dengan tinggi 300 piksel.
-            MapView()
-                .frame(height: 300)
-            
-            // Menampilkan gambar lingkaran.
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
-            // Menampilkan informasi tentang landmark dalam VStack.
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                HStack {
-                    Text("Joshua Tree National Park")
-                    Spacer()
-                    Text("California")
-                }
-                .font(.subheadline)
-                .foregroundStyle(.secondary)
-                
-                Divider() // Garis pemisah.
-                
-                
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("Descriptive text goes here.")
-            }
-            .padding()
-            // Padding untuk konten dalam VStack.
-            
-            Spacer()
-            // Spacer untuk menempatkan konten di bagian bawah halaman.
-        }
+        LandmarkList()
     }
 }
 
--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -9,8 +9,21 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        List(landmarks) { landmark in
-            LandmarkRow(landmark: landmark)
+        NavigationSplitView {
+            // Menampilkan List dari landmarks.
+            List(landmarks) { landmark in
+                // Setiap landmark dalam List dibungkus dengan NavigationLink untuk navigasi ke detail landmark.
+                NavigationLink {
+                    LandmarkDetail()
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            // Mengatur judul navigasi.
+            .navigationTitle("Landmarks")
+        } detail: {
+            // Teks yang ditampilkan di panel detail.
+            Text("Select a Landmark")
         }
     }
 }
