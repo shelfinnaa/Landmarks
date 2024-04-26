9fd8a66ea88da46317d45b5f5bda4dc87617726b
Shelfinna <shelfinna.s@gmail.com>
2024-04-26 08:33:31 +0700

    Chapter 2: Section 6

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Shelfinna on 26/04/24.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
