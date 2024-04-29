372d72610810e7edf8d27695c744a15d43b12441
Shelfinna <shelfinna.s@gmail.com>
2024-04-29 11:34:26 +0700

    Chapter 4: Creating a WatchOS App

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -17,6 +17,10 @@ struct LandmarksApp: App {
             ContentView()
                 .environment(modelData)
         }
+        
+#if os(watchOS)
+        WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+#endif
     }
     
 }
---
+++ WatchLandmarks Watch App/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Shelfinna on 27/04/24.
+//
+
+import SwiftUI
+import UserNotifications
+
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .task {
+                let center = UNUserNotificationCenter.current()
+                _ = try? await center.requestAuthorization(
+                    options: [.alert, .sound, .badge]
+                )
+            }
+    }
+}
+
+#Preview {
+    ContentView()
+        .environment(ModelData())
+}
